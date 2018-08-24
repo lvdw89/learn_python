07284b93b2c4e92b367e67b1414cadc7ce6e942d
Lorenz Vandewaeter <lorenzvandewaeter@outlook.com>
2018-08-24 19:58:26 +0100

    up to ch4

---
+++ docs_python_org/ch4_control_flows.py
@@ -0,0 +1,27 @@
+#===============================================================================
+# x = int (input('please enter an integer:'))
+# 
+# if x > 5 :
+#     print (x)
+#     
+# elif x==4:
+#  print ('blabla')
+# 
+# else:
+#     print(5)
+#===============================================================================
+
+
+for i in range (5):
+    print (i)
+    
+range(5, 10)
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+
+for i in range (len(a )):
+    print (i,a[i])
+    
+list (range(5))
+
+print (list)
---
+++ docs_python_org/fibonacci.py
@@ -0,0 +1,16 @@
+
+ 
+
+
+ 
+def fib (n):
+    
+    a , b = 0, 1;
+
+    while a < n :
+
+        a = b ;
+        b = a+b;
+        print ('value of a is' , a , 'value of b is', b)
+ 
+fib(20)
